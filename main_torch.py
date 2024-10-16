640da3f6c06031b47d374d1d3047a1f6d012e39d
denis <dsenkin@rkrk.uni-kl.de>
2024-10-16 02:22:53 +0200

    add continue from ckpt

--- main_torch.py
+++ main_torch.py
@@ -3,6 +3,11 @@ import torch
 from lightning.pytorch.cli import LightningCLI
 
 
+class NanoCLI(LightningCLI):
+    def add_arguments_to_parser(self, parser):
+        parser.add_argument("--continue", type=str, default=None, help="Continue training from this checkpoint")
+
+
 if __name__ == '__main__':
     torch.set_float32_matmul_precision('medium')
     cli = LightningCLI(run=False)  # True by default
