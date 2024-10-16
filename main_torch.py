42901f897e622e314a679b18608c4c486e92510c
denis <dsenkin@rkrk.uni-kl.de>
2024-10-16 02:20:11 +0200

    add continue from ckpt

--- main_torch.py
+++ main_torch.py
@@ -7,4 +7,7 @@ if __name__ == '__main__':
     torch.set_float32_matmul_precision('medium')
     cli = LightningCLI(run=False)  # True by default
     # you'll have to call fit yourself:
-    cli.trainer.fit(cli.model, datamodule=cli.datamodule)
+    ckpt_path = None
+    if 'continue' in cli.config:
+        ckpt_path = cli.config['continue']
+    cli.trainer.fit(cli.model, datamodule=cli.datamodule, ckpt_path=ckpt_path)
